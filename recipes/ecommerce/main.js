3dbc8700d2cb58bebbb650e40b0b94a431964034
Deepak Grover <deepakgrover333@gmail.com>
2016-11-30 19:26:09 +0530

    add onboarding and related services

--- recipes/ecommerce/main.js
+++ recipes/ecommerce/main.js
@@ -1,4 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Onboarding from 'appbase-onboarding';
 
-ReactDOM.render(<p>works</p>, document.getElementById('onboarding-container'));
+import AppCreation from './steps/AppCreation';
+
+const steps = [
+	'Create an app',
+	'Define data model',
+	'Index data',
+	'Ecommerce App UI'
+];
+
+ReactDOM.render(
+	<Onboarding brandImage='' steps={steps}>
+		<AppCreation key={0} />
+	</Onboarding>,
+	document.getElementById('onboarding-container')
+);
